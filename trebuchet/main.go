852d814ea0db024773f47e7c892627cf4569af06
Cole Cunningham <me@colecunningham.dev>
2023-12-02 14:58:02 -0600

    add CLI flag for input file path

--- trebuchet/main.go
+++ trebuchet/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var (
+	inputFilepath = flag.String("in", "", "Specify the file to use for input. Defaults to reading from STDIN.")
+)
+
+func init() {
+	flag.BoolFunc("h", "Displays usage", func(_ string) error { flag.CommandLine.Usage(); return nil })
+}
 
 func main() {
-	log.Println("Hello, World!")
+	flag.Parse()
+	log.Println("--- Day 1:", "Trebuchet!? ---")
+	validateInputFilePath(inputFilepath)
+	log.Println("reading input from file:", *inputFilepath)
+}
+
+func validateInputFilePath(s *string) {
+	if *s == "" {
+		log.Println("reading input from stdin")
+		return
+	}
+	p := filepath.Clean(*s)
+	info, err := os.Stat(p)
+	if err != nil {
+		log.Fatalf("error reading input file %q: %v\n", *inputFilepath, err)
+	}
+	if info.IsDir() {
+		log.Fatalln("input file path cannot be a directory")
+	}
+	s = &p
 }
